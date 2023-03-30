29c66d5a1aaf52763ed012e3aee0863fbc56c022
Alex <example@gmail.com>
2023-03-30 22:06:57 +0600

    if switch

--- JS/app.js
+++ JS/app.js
@@ -17,3 +17,42 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+if (4) {
+    console.log('Ok');
+} else {
+    console.log('Error');
+}
+
+//----Условие----- if--else----------else if-----
+//------ switch - case - break - default
+
+if (number < 49) {
+    console.log('Error');
+} else if (number > 100) {
+    console.log("Много");
+} else {
+    console.log('OK');
+}
+
+(number === 50) ? console.log('OK') : console.log("Error");
+
+
+
+const number = 50;
+
+switch (number) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 51:
+        console.log('верно');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+
+}
